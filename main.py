0f729c06cd3ab63835bdfb290ebdf38a9e13b0ec
Michal <s22446@pjwstk.edu.pl>
2022-06-17 13:32:09 +0200

    Added time tracking

--- main.py
+++ main.py
@@ -4,26 +4,37 @@ import encrypter.encrypter as encrypter
 import min_heap.min_heap as heap
 import config
 
+import time
+
 if __name__ == '__main__':
-    file = file_handler.opener(config.ENCRYPTION_FILE_PATH)
+    file = file_handler.opener(config.BIG_ENCRYPTION_FILE_PATH)
     file_content = file.read()
     frequency_dictionary = encrypter.count_frequency(file_content)
     original_dictionary = frequency_dictionary.copy()
     letters_array = list(frequency_dictionary.keys())
 
-    print("DEBUG - frequency dictionary: ", frequency_dictionary)
-    print("DEBUG - letters before min heapify: ", letters_array)
+    print("Frequency dictionary: ", frequency_dictionary)
+
+    print("\nCharacters before min heapify: ", letters_array)
 
     heaper = heap.MinHeaper(frequency_dictionary, letters_array, original_dictionary)
-
     heaper.min_heap()
 
-    print("DEBUG - letters after min heapify: ", heaper.array, "\n")
+    print("Characters after min heapify: ", heaper.array, "\n")
 
+    print("Starting encoding")
+
+    start_time = time.time()
     codes = encrypter.create_codes_heap(heaper)
-
     encoded_message = encrypter.encode_message(codes, file_content)
+    end_time = time.time()
 
+    print("Text encoded successfully; time: ", end_time - start_time)
+
+    print("\nWriting to file")
+    start_time = time.time()
     file_handler.write_codes_to_text_file("output/codes.txt", codes)
     file_handler.write_encoded_message_to_text_file("output/encoded_message.txt", codes, encoded_message)
     file_handler.write_encoded_message_to_bin_file("output/encoded_message.bin", codes, encoded_message)
+    end_time = time.time()
+    print("Encoded message successfully written in files; time: ", end_time - start_time)
